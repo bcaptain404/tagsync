cc8314da6183499b1716a575f62777e232202938
Brandon@Laptop <bcaptain404@gmail.com>
2025-07-10 14:48:51 -0400

    tsls.sh

--- tsls.sh
+++ tsls.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # v0.1.2
-# List only TAGGED files/dirs as ls would, using ls for formatting.
+# Only lists tagged files/dirs as ls would, using ls for formatting.
+# For tagged dirs: lists only the dir itself, not its contents.
 
 XATTR_NAME="user.backup_id"
 
@@ -11,7 +12,7 @@ TagSync: tsls.sh v0.1.2
 Usage: $0 [OPTIONS] [files-or-dirs...]
   -h, --help      Show this help.
   [ls-args...]    Any arguments not recognized as flags are passed to ls.
-  Only tagged files/dirs are listed.
+  Only tagged files/dirs are listed. Tagged dirs print the dir only (not contents).
 EOF
 }
 
@@ -28,32 +29,47 @@ done
 
 if [[ ${#LS_PATHS[@]} -eq 0 ]]; then
   # No files/dirs specified: use '.'
-  # If '.' is tagged, print '.'
   if getfattr --only-values -n "$XATTR_NAME" "." &>/dev/null; then
-    ls "${LS_FLAGS[@]}" .
+    ls "${LS_FLAGS[@]}" -d .
     exit 0
   fi
   # Otherwise, only list tagged files in the current dir
-  TO_LS=()
+  TO_LS_FILES=()
+  TO_LS_DIRS=()
   while IFS= read -r f; do
     if getfattr --only-values -n "$XATTR_NAME" "$f" &>/dev/null; then
-      TO_LS+=("$f")
+      if [[ -d "$f" && ! -L "$f" ]]; then
+        TO_LS_DIRS+=("$f")
+      else
+        TO_LS_FILES+=("$f")
+      fi
     fi
   done < <(ls -A1)
-  if [[ ${#TO_LS[@]} -gt 0 ]]; then
-    ls "${LS_FLAGS[@]}" "${TO_LS[@]}"
+  if [[ ${#TO_LS_FILES[@]} -gt 0 ]]; then
+    ls "${LS_FLAGS[@]}" "${TO_LS_FILES[@]}"
+  fi
+  if [[ ${#TO_LS_DIRS[@]} -gt 0 ]]; then
+    ls "${LS_FLAGS[@]}" -d "${TO_LS_DIRS[@]}"
   fi
   exit 0
 fi
 
-# Some files/dirs were specified. List only those that are tagged.
-TO_LS=()
+# Some files/dirs were specified.
+TO_LS_FILES=()
+TO_LS_DIRS=()
 for ARG in "${LS_PATHS[@]}"; do
   if getfattr --only-values -n "$XATTR_NAME" "$ARG" &>/dev/null; then
-    TO_LS+=("$ARG")
+    if [[ -d "$ARG" && ! -L "$ARG" ]]; then
+      TO_LS_DIRS+=("$ARG")
+    else
+      TO_LS_FILES+=("$ARG")
+    fi
   fi
 done
 
-if [[ ${#TO_LS[@]} -gt 0 ]]; then
-  ls "${LS_FLAGS[@]}" "${TO_LS[@]}"
+if [[ ${#TO_LS_FILES[@]} -gt 0 ]]; then
+  ls "${LS_FLAGS[@]}" "${TO_LS_FILES[@]}"
+fi
+if [[ ${#TO_LS_DIRS[@]} -gt 0 ]]; then
+  ls "${LS_FLAGS[@]}" -d "${TO_LS_DIRS[@]}"
 fi
