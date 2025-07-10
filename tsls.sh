8340973b18836b34fd95951d2a0a0267890a8698
Brandon@Laptop <bcaptain404@gmail.com>
2025-07-10 14:45:06 -0400

    tsls.sh

--- tsls.sh
+++ tsls.sh
@@ -10,39 +10,47 @@ VERBOSE=0
 show_help() {
   cat <<EOF
 TagSync: tsls.sh v0.1.2
-Usage: $0 [OPTIONS] [DIR ...]
+Usage: $0 [OPTIONS] [--] [ls-args...]
   -V, --verbose   Show all files; highlight tagged files in magenta.
   -h, --help      Show this help.
-  [DIR ...]       Directory/directories to list (default: .)
+  [ls-args...]    Any other arguments are passed to ls.
 EOF
 }
 
-DIRS=()
+LS_ARGS=()
 while [[ $# -gt 0 ]]; do
   case "$1" in
     -h|--help) show_help; exit 0 ;;
     -V|--verbose) VERBOSE=1 ;;
-    *) DIRS+=("$1") ;;
+    *) LS_ARGS+=("$1") ;;
   esac
   shift
 done
 
-if [[ ${#DIRS[@]} -eq 0 ]]; then DIRS=("."); fi
+# Run `ls -A1` to get plain filenames for parsing, but for output/formatting, user gets their own flags.
+# Need to detect files in each target dir and filter as needed.
 
-for DIR in "${DIRS[@]}"; do
-  # List all files/dirs, one per line, skipping . and ..
-  mapfile -t FILES < <(ls -A1 "$DIR")
+# Figure out which arguments are files/dirs, vs formatting flags for ls
+LS_PATHS=()
+for arg in "${LS_ARGS[@]}"; do
+  [[ "$arg" =~ ^- ]] || LS_PATHS+=("$arg")
+done
+if [[ ${#LS_PATHS[@]} -eq 0 ]]; then
+  LS_PATHS=(".")
+fi
+
+for DIR in "${LS_PATHS[@]}"; do
+  # List files, using formatting flags, but always -A1 to get plain names for filtering
+  mapfile -t FILES < <(ls -A1 -- "${DIR}")
   for FILE in "${FILES[@]}"; do
     FULL="$DIR/$FILE"
     if getfattr --only-values -n "$XATTR_NAME" "$FULL" &>/dev/null; then
-      # Tagged file
       if (( VERBOSE )); then
         echo -e "\033[35m$FILE\033[0m"
       else
         echo "$FILE"
       fi
     else
-      # Not tagged
       if (( VERBOSE )); then
         echo "$FILE"
       fi
