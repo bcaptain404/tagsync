a965b227d18b8bd3d412ddbac8fce84f9158bfb7
Brandon@Laptop <bcaptain404@gmail.com>
2025-07-10 14:47:19 -0400

    tsls.sh

--- tsls.sh
+++ tsls.sh
@@ -1,59 +1,59 @@
 #!/usr/bin/env bash
 
 # v0.1.2
-# List only TAGGED files by default.
-# With -V/--verbose, show all files; tagged ones in magenta.
+# List only TAGGED files/dirs as ls would, using ls for formatting.
 
 XATTR_NAME="user.backup_id"
-VERBOSE=0
 
 show_help() {
   cat <<EOF
 TagSync: tsls.sh v0.1.2
-Usage: $0 [OPTIONS] [--] [ls-args...]
-  -V, --verbose   Show all files; highlight tagged files in magenta.
+Usage: $0 [OPTIONS] [files-or-dirs...]
   -h, --help      Show this help.
-  [ls-args...]    Any other arguments are passed to ls.
+  [ls-args...]    Any arguments not recognized as flags are passed to ls.
+  Only tagged files/dirs are listed.
 EOF
 }
 
-LS_ARGS=()
+LS_FLAGS=()
+LS_PATHS=()
 while [[ $# -gt 0 ]]; do
   case "$1" in
     -h|--help) show_help; exit 0 ;;
-    -V|--verbose) VERBOSE=1 ;;
-    *) LS_ARGS+=("$1") ;;
+    -*) LS_FLAGS+=("$1") ;;
+    *) LS_PATHS+=("$1") ;;
   esac
   shift
 done
 
-# Run `ls -A1` to get plain filenames for parsing, but for output/formatting, user gets their own flags.
-# Need to detect files in each target dir and filter as needed.
-
-# Figure out which arguments are files/dirs, vs formatting flags for ls
-LS_PATHS=()
-for arg in "${LS_ARGS[@]}"; do
-  [[ "$arg" =~ ^- ]] || LS_PATHS+=("$arg")
-done
 if [[ ${#LS_PATHS[@]} -eq 0 ]]; then
-  LS_PATHS=(".")
+  # No files/dirs specified: use '.'
+  # If '.' is tagged, print '.'
+  if getfattr --only-values -n "$XATTR_NAME" "." &>/dev/null; then
+    ls "${LS_FLAGS[@]}" .
+    exit 0
+  fi
+  # Otherwise, only list tagged files in the current dir
+  TO_LS=()
+  while IFS= read -r f; do
+    if getfattr --only-values -n "$XATTR_NAME" "$f" &>/dev/null; then
+      TO_LS+=("$f")
+    fi
+  done < <(ls -A1)
+  if [[ ${#TO_LS[@]} -gt 0 ]]; then
+    ls "${LS_FLAGS[@]}" "${TO_LS[@]}"
+  fi
+  exit 0
 fi
 
-for DIR in "${LS_PATHS[@]}"; do
-  # List files, using formatting flags, but always -A1 to get plain names for filtering
-  mapfile -t FILES < <(ls -A1 -- "${DIR}")
-  for FILE in "${FILES[@]}"; do
-    FULL="$DIR/$FILE"
-    if getfattr --only-values -n "$XATTR_NAME" "$FULL" &>/dev/null; then
-      if (( VERBOSE )); then
-        echo -e "\033[35m$FILE\033[0m"
-      else
-        echo "$FILE"
-      fi
-    else
-      if (( VERBOSE )); then
-        echo "$FILE"
-      fi
-    fi
-  done
+# Some files/dirs were specified. List only those that are tagged.
+TO_LS=()
+for ARG in "${LS_PATHS[@]}"; do
+  if getfattr --only-values -n "$XATTR_NAME" "$ARG" &>/dev/null; then
+    TO_LS+=("$ARG")
+  fi
 done
+
+if [[ ${#TO_LS[@]} -gt 0 ]]; then
+  ls "${LS_FLAGS[@]}" "${TO_LS[@]}"
+fi
